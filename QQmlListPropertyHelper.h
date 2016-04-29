b613ee261df2b709d7c752bb852b7b3aa1580dc3
Thomas BOUTROUE <thebootroo@gmail.com>
2016-04-29 04:09:33 +0200

    Complete refactor of the QML_LIST_PROPERTY
    Now uses a smart subclass of QQmlListProperty to provide API
    
    (this allow 100% automatic list handling with clean code)

--- QQmlListPropertyHelper.h
+++ QQmlListPropertyHelper.h
@@ -4,33 +4,59 @@
 #include <QList>
 #include <QQmlListProperty>
 
-#define QML_LIST_PROPERTY(CLASS, NAME, TYPE) \
+template<class ObjType> class QQmlSmartList : public QQmlListProperty<ObjType> {
+public:
+    explicit QQmlSmartList (QObject * object)
+        : QQmlListProperty<ObjType>
+          (object,
+           this,
+           &QQmlSmartList<ObjType>::callbackAppend,
+           &QQmlSmartList<ObjType>::callbackCount,
+           &QQmlSmartList<ObjType>::callbackAt,
+           &QQmlSmartList<ObjType>::callbackClear)
+    { }
+
+    typedef QList<ObjType *>          CppListType;
+    typedef QQmlSmartList<ObjType>    SmartListType;
+    typedef QQmlListProperty<ObjType> QmlListPropertyType;
+
+    CppListType list;
+
+    static int callbackCount (QmlListPropertyType * prop) {
+        SmartListType * self = static_cast<SmartListType *> (prop->data);
+        return (self != Q_NULLPTR ? self->list.count () : 0);
+    }
+
+    static void callbackClear (QmlListPropertyType * prop) {
+        SmartListType * self = static_cast<SmartListType *> (prop->data);
+        if (self != Q_NULLPTR) {
+            self->list.clear ();
+        }
+    }
+
+    static void callbackAppend (QmlListPropertyType * prop, ObjType * obj) {
+        SmartListType * self = static_cast<SmartListType *> (prop->data);
+        if (self != Q_NULLPTR && obj != Q_NULLPTR) {
+            self->list.append (obj);
+        }
+    }
+
+    static ObjType * callbackAt (QmlListPropertyType * prop, int idx) {
+        SmartListType * self = static_cast<SmartListType *> (prop->data);
+        return (self != Q_NULLPTR ? self->list.at (idx) : Q_NULLPTR);
+    }
+};
+
+#define QML_LIST_PROPERTY(TYPE, NAME) \
+    private: \
+        Q_PROPERTY (QQmlListProperty<TYPE> NAME READ get_##NAME CONSTANT) \
     public: \
-        static int NAME##_count (QQmlListProperty<TYPE> * prop) { \
-            CLASS * instance = qobject_cast<CLASS *> (prop->object); \
-            return (instance != NULL ? instance->m_##NAME.count () : 0); \
-        } \
-        static void NAME##_clear (QQmlListProperty<TYPE> * prop) { \
-            CLASS * instance = qobject_cast<CLASS *> (prop->object); \
-            if (instance != NULL) { \
-                instance->m_##NAME.clear (); \
-            } \
-        } \
-        static void NAME##_append (QQmlListProperty<TYPE> * prop, TYPE * obj) { \
-            CLASS * instance = qobject_cast<CLASS *> (prop->object); \
-            if (instance != NULL && obj != NULL) { \
-                instance->m_##NAME.append (obj); \
-            } \
-        } \
-        static TYPE * NAME##_at (QQmlListProperty<TYPE> * prop, int idx) { \
-            CLASS * instance = qobject_cast<CLASS *> (prop->object); \
-            return (instance != NULL ? instance->m_##NAME.at (idx) : NULL); \
-        } \
-        QList<TYPE *> get_##NAME##s (void) const { \
+        QQmlSmartList<TYPE> & get_##NAME (void) { \
             return m_##NAME; \
         } \
     private: \
-        QList<TYPE *> m_##NAME;
+        QQmlSmartList<TYPE> m_##NAME;
+
 
 #endif // QQMLLISTPROPERTYHELPER
 
