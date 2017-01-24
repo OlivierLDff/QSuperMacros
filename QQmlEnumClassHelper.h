cdaeae964bbaea04ea16e90471a075beb9beaea1
Thomas BOUTROUE <thebootroo@gmail.com>
2017-01-24 17:29:13 +0100

    Better implementation of QML_ENUM_CLASS (lighter and safer)

--- QQmlEnumClassHelper.h
+++ QQmlEnumClassHelper.h
@@ -9,13 +9,20 @@
 #   define QML_EXPORT_ENUM Q_ENUMS
 #endif
 
-#define QML_ENUM_CLASS(name, ...) \
-    class name : public QObject { \
+#define QML_ENUM_CLASS(NAME, ...) \
+    struct NAME { \
         Q_GADGET \
-    public: \
-        enum Type { __VA_ARGS__ }; \
-        QML_EXPORT_ENUM (Type) \
-    };
+        public: \
+            enum Type { \
+                __VA_ARGS__ \
+            }; \
+            QML_EXPORT_ENUM (Type) \
+        private: \
+            explicit NAME (void) { } \
+            NAME (const NAME &); \
+            NAME & operator= (const NAME &); \
+    }; \
+    Q_DECLARE_METATYPE (NAME::Type)
 
 class _Test_QmlEnumClass_ { Q_GADGET }; // NOTE : to avoid "no suitable class found" MOC note
 
