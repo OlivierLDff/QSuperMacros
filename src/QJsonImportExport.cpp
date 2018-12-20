8990588d138edcd927b26a4ca006d9de8a0fa5b0
OlivierLdff <olivier.ldff@gmail.com>
2018-12-20 22:41:52 +0100

    src/QJsonImportExport

---
+++ src/QJsonImportExport.cpp
@@ -0,0 +1,50 @@
+// ─────────────────────────────────────────────────────────────
+//					INCLUDE
+// ─────────────────────────────────────────────────────────────
+
+#include <QJsonImportExport.h>
+
+// ─────────────────────────────────────────────────────────────
+//					DECLARATION
+// ─────────────────────────────────────────────────────────────
+
+QSUPER_MACROS_USING_NAMESPACE;
+
+// ─────────────────────────────────────────────────────────────
+//					FUNCTIONS
+// ─────────────────────────────────────────────────────────────
+
+bool QJsonExportable::JsonSave(const QUrl& filepath)
+{
+	QFile saveFile(filepath.toLocalFile());
+
+	if (!saveFile.open(QIODevice::WriteOnly))
+	{
+		qWarning("Couldn't open Json save file.");
+		return false;
+	}
+
+	QJsonObject jsonObject;
+	JsonWrite(jsonObject);
+	QJsonDocument saveDoc(jsonObject);
+	saveFile.write(saveDoc.toJson());
+
+	return true;
+}
+
+bool QJsonImportable::JsonLoad(const QUrl& filepath)
+{
+	QFile loadFile(filepath.toLocalFile());
+
+	if (!loadFile.open(QIODevice::ReadOnly))
+	{
+		qWarning("Couldn't open Json file to load.");
+		return false;
+	}
+
+	const QByteArray saveData = loadFile.readAll();
+	QJsonDocument loadDoc(QJsonDocument::fromJson(saveData));
+	JsonRead(loadDoc.object());
+
+	return true;
+}
---
+++ src/QJsonImportExport.h
@@ -0,0 +1,68 @@
+/** 
+ * \file QJsonImportExport.h
+ * \brief Common define for the library
+ */
+#ifndef __QJSON_IMPORT_EXPORT_HPP__
+#define __QJSON_IMPORT_EXPORT_HPP__
+
+// ─────────────────────────────────────────────────────────────
+//					INCLUDE
+// ─────────────────────────────────────────────────────────────
+
+// C Header
+
+// C++ Header
+
+// Qt Header
+#include <QJsonObject>
+#include <QJsonDocument>
+#include <QFile>
+#include <QUrl>
+
+// Dependancies Header
+
+// Application Header
+#include <QSuperMacros.h>
+
+// ─────────────────────────────────────────────────────────────
+//					DECLARATION
+// ─────────────────────────────────────────────────────────────
+
+QSUPER_MACROS_NAMESPACE_START
+
+/** An object that can be export to a json document */
+class QSUPER_MACROS_API_ QJsonExportable
+{
+public:
+	/** Public virtual Destructor */
+	virtual ~QJsonExportable() = default;
+	/** Save the object in the filepath.
+	 * \return If the save succeed
+	 */
+	virtual bool JsonSave(const QUrl& filepath);
+	/** Dump the object in the json object */
+	virtual void JsonWrite(QJsonObject &json) const {};
+};
+
+
+/** An object that can be import from a json document */
+class QSUPER_MACROS_API_ QJsonImportable
+{
+public:
+	/** Public virtual Destructor */
+	virtual ~QJsonImportable() = default;
+	/** Load from a Json file. \return If the load was a success */
+	virtual bool JsonLoad(const QUrl& filepath);
+	/** Inflate from the json object */
+	virtual void JsonRead(const QJsonObject &json) {};
+};
+
+
+/** An object that can be import and export to and from json format */
+class QSUPER_MACROS_API_ QJsonImportExport : public QJsonExportable, public QJsonImportable
+{
+};
+
+QSUPER_MACROS_NAMESPACE_END
+
+#endif
